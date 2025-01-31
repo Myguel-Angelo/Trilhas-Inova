63c25055bd14a855150ae274c7b30a39cda32979
Myguel-Angelo <angelo.nunes576@gmail.com>
2025-01-31 19:43:33 -0300

    resolução: desafio 1

--- desafio-1/app.js
+++ desafio-1/app.js
@@ -1,1 +1,153 @@
 // Estou criando tudo em apenas esse arquivo app.js
+
+// 1°
+/*
+let nome = 'Myguel Ângelo';
+console.log(nome);
+*/
+
+
+// 2°
+/*  
+let idade = 25;
+let altura = 1.75;
+console.log(idade, altura);
+*/
+
+
+// 3°
+/*
+let preco = 50;
+let desconto = 0.2;
+let valorComDesconto = preco-(preco*desconto);
+console.log(valorComDesconto);
+*/
+
+
+// 4°
+/*  
+let temperatura = 30;  
+if (temperatura > 25) {  
+    console.log("Está calor!");  
+} else {  
+    console.log("Está fresco!");  
+}  
+*/
+
+
+// 5°
+/*  
+let idade = 18;  // Eu tenho 18 kkkkk
+if (idade >= 18) {  
+    console.log("Você é maior de idade");  
+} else {  
+    console.log("Você é menor de idade");  
+}  
+*/
+
+
+// 6°
+/*
+let nota = 6;  /// Em homenagem a questão 6
+if (nota >= 7) {  
+    console.log("Aprovado");  
+} else if (nota >= 5) {  
+    console.log("Recuperação");  
+} else {  
+    console.log("Reprovado");  
+}  
+*/
+
+
+// 7°
+/*  
+let numero1 = 10;  
+let numero2 = 20;  
+if (numero1 === numero2) {  
+    console.log("Os números são iguais");  
+} else {  
+    console.log("Os números são diferentes");  
+}  
+*/
+
+
+// 8°
+/*  
+let nome = "Ângelo Nunes";  
+let idade = 18;  
+console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos");  
+*/
+
+
+// 9°
+/*
+let cont = 1;
+while(cont<=10) {
+    console.log(cont)
+    cont++;
+}
+*/
+
+// 10°
+/*  
+let numero = prompt("Digite um número:");
+while(numero!=5) {
+    console.log(numero);
+    numero = prompt("Digite um número:");  
+}  
+console.log("Você digitou 5, encerrando...");  
+*/
+
+
+// 11°
+/*
+let multi = 0;
+let number = 7;
+while(multi<=10){
+    console.log(`${number} x ${multi} =`, multi*number);
+    multi++;
+}
+*/
+
+
+// 12°  
+/*
+let number = 0;
+while(number<=20){
+    console.log(number)
+    number+=2;
+}
+*/
+
+
+// 13°  
+/*
+function calcularAreaCirculo(raio) {  
+    return Math.PI * (raio**2); 
+    //Pensei em usar 3.14, mas achei esse Math.PI na net e resulvi usar
+}  
+console.log(calcularAreaCirculo(5));  
+*/
+
+
+// 14°  
+/*
+// Função para somar dois números (A e B)  
+function somar(a, b) {  
+    return a + b; // retorna a soma de a+b
+}  
+let resultado = somar(14, 41);  //variável que recebe o retorno da função
+console.log(resultado);  // Exibindo o resultado
+*/
+
+
+// 15°  
+/*
+function somarNumeros(x, y) {  
+    return x + y;  
+}  
+let x = 15;
+let y = 51;
+let z = somarNumeros(x, y);  
+console.log(z);  
+*/
